e2ab82aec19eb5161aa5b91c09bf112cd5fb293f
Mateusz Przybyla <inz.mateusz.przybyla@gmail.com>
2017-03-15 16:36:52 +0100

    Added hello_world program

---
+++ src/hello_world.cpp
@@ -0,0 +1,13 @@
+#include <ros/ros.h>
+#include <iostream>
+
+int main(int argc, char** argv) {
+  //ros::init(argc, argv, "hello_world");
+  //ros::NodeHandle nh;
+
+  std::cout << "Hello, world!" << std::endl;
+
+  //ros::spin();
+
+  return 0;
+}
